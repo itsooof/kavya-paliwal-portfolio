6bcd67a2a64853ec47c6c9d12ec78da2f2fc723f
Kavya Paliwal <kpaliwal2002@gmail.com>
2023-07-12 00:26:13 +0530

    Update

--- src/components/ExternalComponents/demoProjectArray.jsx
+++ src/components/ExternalComponents/demoProjectArray.jsx
@@ -1,19 +1,10 @@
 import img1 from "../../../public/assets/Portfolio.png";
-import img2 from "../../assets/melanoma-detection.jpeg";
-import img3 from "../../assets/quilting.png";
-import img4 from "../../assets/obstacle-avoiding.jpeg";
-import img5 from "../../assets/calculator.png";
-import img6 from "../../assets/phishing.png";
+import img2 from "../../../public/assets/melanoma-detection.jpeg";
+import img3 from "../../../public/assets/quilting.png";
+import img4 from "../../../public/assets/obstacle-avoiding.jpeg";
+import img5 from "../../../public/assets/calculator.png";
+import img6 from "../../../public/assets/phishing.png";
 
-/*
-Image Source:
-    Img1: https://pin.it/4cuXKYP || https://in.pinterest.com/the_xz/
-    Img2: https://pin.it/4thAhOe || https://www.reddit.com/r/Cyberpunk/comments/c6nqbw/cyborg_girl_by_danny_kim/
-    Img3: Link Deleted
-    Img4: https://pin.it/7k0e79e || https://in.pinterest.com/wattpad/
-    Img5: https://in.pinterest.com/wattpad/
-    Img6: https://in.pinterest.com/wattpad/ || 
-*/
 
 export const data = [
   {
